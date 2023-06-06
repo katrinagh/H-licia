6648ede71026a1dd4444c6a7ef9f3fc332e33ce1
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-06 14:13:32 +0200

    fixing server

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_06_111154) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "article_orders", force: :cascade do |t|
     t.integer "quantity"
     t.integer "order_id", null: false

c987364dddbd22a4af50c3fa398e85d683ea81da
Katrina Gherzeddine <katgherzeddine@gmail.com>
2023-06-06 16:26:32 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # creating users
 puts "Cleaning database..."
-Article.User.destroy_all
+Store.destroy_all
+Article.destroy_all
 
 
 titles = ["Amavita", "SUN STORE", "PharmaSuisse", "BENU", "TopPharm", "Pharmacie Bien-Être",
@@ -32,8 +33,7 @@ estimated_delivery_times =["10-15min", "15-20min", "10-20min"]
 delivery_fees =["2chf", "4chf", "5chf", "7chf"]
 
 puts "Creating new pharmacies"
-8.times do
-    User.create!(
+    Store.create!(
     title: titles[rand(1..8)],
     description:  descriptions[rand(1..8)],
     image: images[rand(1..8)],
@@ -41,18 +41,96 @@ puts "Creating new pharmacies"
     estimated_delivery_time: estimated_delivery_tims[rand(1..3)],
     delivery_fee: delivery_fees[rand(1..4)]
     )
-end
 
 
 
-puts "Creating new articles" do
-    User.create!(
-    description_of_article: "cpr 500 mg blist 16 pce",
-    title_of_article: "Dafalgan",
-    weight: "10g",
-    category: "Pain",
-    price: "CHF 2.50"
+  Article.create!(
+  description: "cpr 500 mg blister 16 pce",
+  title: "Dafalgan",
+  weight: "10g",
+  category: "Pain",
+  price: "CHF 2.50"
     )
-end
+  puts "Creating new #{articles.title}"
 
-puts "Finished toilet papers"
+  Article.create!(
+  description: "Probi-intestis caps 40 pce",
+  title: "Vitafor",
+  weight: "20g",
+  category: "Digestion",
+  price: "CHF 48.95"
+  )
+  puts "Creating new #{articles.title}"
+
+
+  Article.create!(
+  description: "fl spray 30 ml",
+  title: "Bucco Tantum",
+  weight: "40g",
+  category: "Coolings",
+  price: "CHF 14.95"
+  )
+  puts "Creating new #{articles.title}"
+
+  Article.create!(
+  description: "Aqueous solution colorless aqueous sol spr 30 ml",
+  title: "Merfen",
+  weight: "40g",
+  category: "Skin & Hair",
+  price: "CHF 9.40"
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "Dry eyes gtt opht monodos 0.35 ml",
+  title: "Collypan",
+  weight: "15g",
+  category: "Ears & Eyes",
+  price: "CHF 20.50"
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "Classic gel tb 200 ml",
+  title: "Perskindol",
+  weight: "210g",
+  category: "Pain",
+  price: "CHF 33.95".
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "Earwax fl 10 ml",
+  title: "Otalgan",
+  weight: "20g",
+  category: "Ears & Eyes",
+  price: "CHF 10.20"
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "Caps blister 10 pc",
+  title: "Itinerol",
+  weight: "11g",
+  category: "Digestion",
+  price: "CHF 14.20"
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "Pro Sensiderm cream tb 20 g",
+  title: "Bepanthen",
+  weight: "30g",
+  category: "Skin & Hair",
+  price: "CHF 14.50"
+  )
+  puts "Creating new #{articles.title}"
+
+Article.create!(
+  description: "cpr eff orange flavor box 60 pce",
+  title: "Berocca",
+  weight: "55g",
+  category: "Fortifiers",
+  price: "CHF 59.95"
+  )
+  puts "Creating new #{articles.title}"

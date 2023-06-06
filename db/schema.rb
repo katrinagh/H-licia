ac9b0e75914d1c21849b684997e8a0dd7ef5dd7b
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-06 16:47:06 +0200

    Changing column type for Esimated delivery time

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_114039) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_144405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_114039) do
     t.string "description"
     t.string "image_url"
     t.integer "rating"
-    t.time "estimated_delivery_time"
+    t.string "estimated_delivery_time"
     t.integer "delivery_fees"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

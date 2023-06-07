3b029828a148620b3c75bc9ffdd0f0b8ac4e7d55
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-07 11:26:47 +0200

    Updating all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders
+  has_many :articles, through: :orders
+  has_one :article_orders, through: :orders
 end

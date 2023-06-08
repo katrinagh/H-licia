949b4f7776ad2cd8099aa65a417694839f1bc36f
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-08 13:27:13 +0200

    routes and controller

---
+++ app/controllers/article_orders_controller.rb
@@ -0,0 +1,2 @@
+class ArticleOrdersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -9,5 +10,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   resources :stores do
     resources :articles
+  end
+
+  resources :users do
+    resources :orders
+    resources :article_orders
   end
 end

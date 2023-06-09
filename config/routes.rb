af2ef51bf36af23867e49846263e80c1f5f3e39e
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-09 16:16:24 +0200

    trying to fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
 
   root to: 'stores#index'
 
-  resources :orders, only: %i[show destroy]
+  resources :orders
 
   resources :article_orders, only: %i[create destroy show]
   post 'article_orders/:id/add' => 'article_orders#add_quantity', as: 'article_order_add'
   post 'article_orders/:id/reduce' => 'article_orders#reduce_quantity', as: 'article_order_reduce'
 
-  resources :articles, only: %i[index new]
+  resources :articles
   resources :confirmed_orders, only: %i[index show new create]
 
-  resources :stores, only: %i[index show]
+  resources :stores
 end

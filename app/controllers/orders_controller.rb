44c0023330d916f18ae3d967889d51224752f980
simon despres <simondprs62790@hotmail.fr>
2023-06-12 15:09:38 +0200

    setup mapbox geocoding

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,10 @@ class OrdersController < ApplicationController
     @orders = Order.all
   end
 
+  def show
+    @order = Order.find(params[:id])
+  end
+
   def new
     @order = Order.new
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,9 @@ class Order < ApplicationRecord
   validates :delivery_time, presence: true
   validate :unique_for_user
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   private
 
   def unique_for_user

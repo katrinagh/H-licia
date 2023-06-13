bdb80faa2f3c292c7e6464b0625fdd54a674a6ff
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-13 17:24:54 +0200

    working cart

--- app/javascript/controllers/insert_in_cart_controller.js
+++ app/javascript/controllers/insert_in_cart_controller.js
@@ -1,42 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 
+let quantity = -1
+
 // Connects to data-controller="insert-in-cart"
 export default class extends Controller {
-  static targets = ["items", "form", "total"]
-  static values = {
-    index: String
-  }
+  static targets = ["items", "form", "total", "endtotal"]
+
   connect() {
-    // const testValue = this.indexValue
-    // console.log(testValue)
-    // console.log(this.element)
-    // console.log(this.formTarget)
     this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   }
 
   // functions 
-
   send(event) {
-    event.preventDefault()
-    // const totalContent = this.totalTargets[event.params.id]
-    // console.log('test 12', this.totalTargets[event.params.id])
-    console.log('hello')
-    // totalContent.textContent = "200"
-    // console.log('test 17', event.target)
-    // console.log('test 18', event.target.id)
+    let endTotal = 0
+    const totalContent = this.totalTargets[event.params.id]
+    if (quantity == -1) {
+      quantity = event.params.num
+    }
+    let price = document.getElementById('price' + event.params.id)
+    totalContent.textContent = (quantity * parseFloat(price.textContent.split(" CHF")[0])).toFixed(2)
 
-    // console.log('target event 5', event.currentTarget)
+    this.totalTargets.forEach(total => {
+      endTotal = parseFloat(total.textContent) + endTotal
+    });
+    // console.log(endTotal)
+    this.endtotalTarget.textContent = endTotal.toFixed(2)
+  }
 
-    // const url = this.formTarget.action
-    // const options = {
-    //   method: "POST",
-    //   headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
-    //   body: new FormData(this.formTarget)
-    // }
-    // fetch(url, options)
-    //   .then(response => response.json())
-    //   .then((data) => {
-    //     console.log(data)
-    //   })
+  retrieveQuantity(event) {
+    quantity = event.target.value
   }
 }

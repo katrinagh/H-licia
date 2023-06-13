4411c9cb2daa95f347529b7a542e17d590e4b2ef
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-06-13 13:33:46 +0200

    stimulus in progress

--- app/javascript/controllers/insert_in_cart_controller.js
+++ app/javascript/controllers/insert_in_cart_controller.js
@@ -2,11 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="insert-in-cart"
 export default class extends Controller {
-  static targets = ["items", "form"]
+  static targets = ["items", "form", "total"]
+  static values = {
+    index: String
+  }
   connect() {
-    console.log(this.element)
-    console.log(this.itemsTarget)
-    console.log(this.formTarget)
+    // const testValue = this.indexValue
+    // console.log(testValue)
+    // console.log(this.element)
+    // console.log(this.formTarget)
     this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
   }
 
@@ -14,16 +18,25 @@ export default class extends Controller {
 
   send(event) {
     event.preventDefault()
-    const url = this.formTarget.action
-    const options = {
-      method: "POST",
-      headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
-      body: new FormData(this.formTarget)
-    }
-    fetch(url, options)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data)
-      })
+    // const totalContent = this.totalTargets[event.params.id]
+    // console.log('test 12', this.totalTargets[event.params.id])
+    console.log('hello')
+    // totalContent.textContent = "200"
+    // console.log('test 17', event.target)
+    // console.log('test 18', event.target.id)
+
+    // console.log('target event 5', event.currentTarget)
+
+    // const url = this.formTarget.action
+    // const options = {
+    //   method: "POST",
+    //   headers: { "Accept": "application/json", "X-CSRF-Token": this.csrfToken },
+    //   body: new FormData(this.formTarget)
+    // }
+    // fetch(url, options)
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //   })
   }
 }

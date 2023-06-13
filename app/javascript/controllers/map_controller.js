1de2589007e65ddb0f1d55409fef1e1552c36567
simon despres <simondprs62790@hotmail.fr>
2023-06-13 16:01:38 +0200

    jai ajouter du code pour animer le marker orders mais je nais pas encore de websocket, pour le moment... :P

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -39,6 +39,42 @@ export default class extends Controller {
       .setLngLat([this.orderMarkerValue.lng, this.orderMarkerValue.lat])
       .setPopup(popup)
       .addTo(this.map)
+
+    // Define the animation.
+    function animateMarker(timestamp) {
+      const startPoint = [startLng, startLat]; // Point A (coordonnées du point de départ)
+      const endPoint = [endLng, endLat]; // Point B (coordonnées du point d'arrivée)
+      const duration = 32000; // Durée de l'animation en millisecondes - 32secondes
+      const progress = Math.min((timestamp - startTime) / duration, 1); // Calculer la progression de l'animation
+
+      // Calculer les coordonnées du marqueur en fonction de la progression
+      const lng = startPoint[0] + (endPoint[0] - startPoint[0]) * progress;
+      const lat = startPoint[1] + (endPoint[1] - startPoint[1]) * progress;
+
+      marker.setLngLat([lng, lat]); // Mettre à jour la position du marqueur
+
+      marker.addTo(map); // S'assurer que le marqueur est ajouté à la carte
+
+      // Vérifier si l'animation doit se poursuivre
+      if (progress < 1) {
+        // Demander le prochain frame de l'animation
+        requestAnimationFrame(animateMarker);
+      }
+    }
+    // Définir les coordonnées du point de départ (A) et du point d'arrivée (B)
+    // A = Order
+    const startLng = 6.632351;
+    const startLat = 46.517983;
+    // B = User
+    const endLng = 6.57538;
+    const endLat = 46.525021;
+
+    // Obtenir l'horodatage de départ
+    const startTime = performance.now();
+
+    // Démarrer l'animation
+    requestAnimationFrame(animateMarker);
+
   }
 
   #fitMapToMarkers() {

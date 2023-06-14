db7f41780909856d509fee24b15130f20b5667d4
simon despres <simondprs62790@hotmail.fr>
2023-06-14 16:19:58 +0200

    fixed conflicts 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,80 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_083125) do
+ActiveRecord::Schema[7.0].define(version: 20_230_613_083_125) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "article_orders", force: :cascade do |t|
-    t.integer "quantity"
-    t.bigint "order_id", null: false
-    t.bigint "article_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_article_orders_on_article_id"
-    t.index ["order_id"], name: "index_article_orders_on_order_id"
+  create_table 'article_orders', force: :cascade do |t|
+    t.integer 'quantity'
+    t.bigint 'order_id', null: false
+    t.bigint 'article_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_article_orders_on_article_id'
+    t.index ['order_id'], name: 'index_article_orders_on_order_id'
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "image_url"
-    t.string "category"
-    t.float "price"
-    t.integer "weight"
-    t.bigint "store_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["store_id"], name: "index_articles_on_store_id"
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.string 'image_url'
+    t.string 'category'
+    t.float 'price'
+    t.integer 'weight'
+    t.bigint 'store_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['store_id'], name: 'index_articles_on_store_id'
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "number"
-    t.string "delivery_time"
-    t.boolean "confirmed", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float "latitude", default: 46.517983
-    t.float "longitude", default: 6.632351
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  create_table 'orders', force: :cascade do |t|
+    t.string 'number'
+    t.string 'delivery_time'
+    t.boolean 'confirmed', null: false
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.float 'latitude', default: 46.517983
+    t.float 'longitude', default: 6.632351
+    t.index ['user_id'], name: 'index_orders_on_user_id'
   end
 
-  create_table "stores", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.string "image_url"
-    t.integer "rating"
-    t.string "estimated_delivery_time"
-    t.integer "delivery_fees"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'stores', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.string 'image_url'
+    t.integer 'rating'
+    t.string 'estimated_delivery_time'
+    t.integer 'delivery_fees'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "phone_number"
-<<<<<<< HEAD
-    t.string "delivery_address", default: "rue de Lausanne 64, Renens", null: false
-=======
-    t.string "delivery_address"
->>>>>>> master
-    t.float "latitude", default: 46.525021
-    t.float "longitude", default: 6.57538
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'first_name'
+    t.string 'last_name'
+    t.string 'phone_number'
+    t.string 'delivery_address', default: 'rue de Lausanne 64, Renens', null: false
+    t.float 'latitude', default: 46.525021
+    t.float 'longitude', default: 6.57538
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 
-  add_foreign_key "article_orders", "articles"
-  add_foreign_key "article_orders", "orders"
-  add_foreign_key "articles", "stores"
-  add_foreign_key "orders", "users"
+  add_foreign_key 'article_orders', 'articles'
+  add_foreign_key 'article_orders', 'orders'
+  add_foreign_key 'articles', 'stores'
+  add_foreign_key 'orders', 'users'
 end
